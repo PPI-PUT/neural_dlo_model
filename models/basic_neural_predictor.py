50300b665841a23acb89f9c9610ddf35224ab921
pkicki <piotr.m.kicki@gmail.com>
2023-02-22 16:08:15 +0100

    Imporved basic predictor

--- models/basic_neural_predictor.py
+++ models/basic_neural_predictor.py
@@ -19,7 +19,12 @@ class BasicNeuralPredictor(tf.keras.Model):
         ]
 
     def __call__(self, x1, x2, x3, training=True):
-        x = tf.concat([x1, x2, x3], axis=-1)
+        bs = x3.shape[0]
+        x1 = tf.concat([tf.reshape(x, (bs, -1)) for x in x1], axis=-1)
+        x2 = tf.concat([tf.reshape(x, (bs, -1)) for x in x2], axis=-1)
+        x3_ = tf.reshape(x3, (bs, -1))
+        x = tf.concat([x1, x2, x3_], axis=-1)
         for l in self.fc:
             x = l(x)
+        x = tf.reshape(x, (-1, BSplineConstants.n, BSplineConstants.dim)) + x3
         return x
